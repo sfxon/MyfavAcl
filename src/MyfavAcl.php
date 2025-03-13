58fe38e70d053934017c7aaf3d77f3cc87cb916a
sfxon <steve@mindfav.com>
2025-03-13 13:23:30 +0100

    Removed installation of custom fields from installer, because they are not needed anymore.

--- src/MyfavAcl.php
+++ src/MyfavAcl.php
@@ -3,14 +3,10 @@
 namespace Myfav\Acl;
 
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
-use Shopware\Core\System\CustomField\CustomFieldTypes;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\IdSearchResult;
 
 class MyfavAcl extends Plugin {
     /**
@@ -22,7 +18,6 @@ class MyfavAcl extends Plugin {
     public function install(InstallContext $installContext): void
     {
         parent::install($installContext);
-        $this->installCustomFields($installContext->getContext());
     }
 
     /**
@@ -49,74 +44,5 @@ class MyfavAcl extends Plugin {
     public function update(UpdateContext $updateContext): void
     {
         parent::update($updateContext);
-    }
-
-    /**
-     * installCustomFields
-     *
-     * @param  Context $context
-     * @return void
-     */
-    private function installCustomFields(Context $context): void
-    {
-        $customFieldSetRepository = $this->container->get('custom_field_set.repository');
-
-        $customFieldSetRepository->upsert([
-            [
-                'name' => 'myfav_acl_product_customer_group',
-                'config' => [
-                    'label' => [
-                        'en-GB' => 'CustomerGroup-Settings',
-                        'de-DE' => 'Kundengruppen-Einstellungen'
-                    ]
-                ],
-                'customFields' => [
-                    [
-                        'name' => 'myfav_acl_product_customer_group_exclude_sitemap',
-                        'type' => CustomFieldTypes::BOOL,
-                        'config' => [
-                            'label' => [
-                                'en-GB' => 'Exclude article from sitemap',
-                                'de-DE' => 'Artikel von der Sitemap ausschließen'
-                            ],
-                            'type' => 'checkbox',
-                            'componentName' => 'sw-field',
-                            'customFieldType' => 'checkbox',
-                            'customFieldPosition' => 10,
-                        ],
-                        'active' => true
-                    ],
-                ],
-            ]
-        ], $context);
-
-        $customFieldSetRepository->upsert([
-            [
-                'name' => 'myfav_acl_category_customer_group',
-                'config' => [
-                    'label' => [
-                        'en-GB' => 'Category-CustomerGroup-Settings',
-                        'de-DE' => 'Kategorie-Kundengruppen-Einstellungen'
-                    ]
-                ],
-                'customFields' => [
-                    [
-                        'name' => 'myfav_acl_category_customer_group_exclude_sitemap',
-                        'type' => CustomFieldTypes::BOOL,
-                        'config' => [
-                            'label' => [
-                                'en-GB' => 'Exclude category from sitemap',
-                                'de-DE' => 'Kategorie von der Sitemap ausschließen'
-                            ],
-                            'type' => 'checkbox',
-                            'componentName' => 'sw-field',
-                            'customFieldType' => 'checkbox',
-                            'customFieldPosition' => 10,
-                        ],
-                        'active' => true
-                    ],
-                ],
-            ]
-        ], $context);
     }
 }
